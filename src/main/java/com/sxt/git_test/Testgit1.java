e1d45ccce5c9e65e8ebd8dabbddfa3e62e63d64f
tom_glocal <djf_glocal@sxt.com>
2021-10-07 10:40:52 +0800

    host_mix first commit

--- src/main/java/com/sxt/git_test/Testgit1.java
+++ src/main/java/com/sxt/git_test/Testgit1.java
@@ -3,5 +3,6 @@ package com.sxt.git_test;
 public class Testgit1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello host_mix");
     }
 }
